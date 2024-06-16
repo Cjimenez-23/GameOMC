85b4220731c342bef8a0807ec7ac93485fe6710d
JCKaren <christian.jimenez-23@outlook.com>
2024-06-15 22:13:46 -0500

    Creando el motor del juego
    Se dieron vida a los objetos HTML dinamicos (spans) asignadoles una funcion con JS.
    Se crearon los mensajes del juego dependiendo de los eventos y comparaciones con el jugador y el enemigo.

--- Mokepon/mokepon.js
+++ Mokepon/mokepon.js
@@ -1,27 +1,60 @@
 let ataqueJugador = "";
 let ataqueEnemigo = "";
+let resultado = "";
+let vidas_Jugador = 3;
+let vidas_Enemigo = 3;
 
+function combate() {
+    if (ataqueJugador == "FUEGO" && ataqueEnemigo == "TIERRA" ||
+        ataqueJugador == "AGUA" && ataqueEnemigo == "FUEGO" ||
+        ataqueJugador == "AGUA" && ataqueEnemigo == "TIERRA") {
+        resultado = "GANASTE 🎉"
+        vidas_Enemigo--
+    } else if (ataqueJugador == ataqueEnemigo) {
+        resultado = "EMPATE 🤝🏻"
+    } else {
+        resultado = "PERDISTE 👎🏿"
+        vidas_Jugador--
+    }
+
+    mensajes()
+}
+
+function mensajes() {
+    let span_Vidas_Jugador = document.getElementById("vidas_Jugador")
+    let span_Vidas_Enemigo = document.getElementById("vidas_Enemigo")
+    span_Vidas_Jugador.innerHTML = vidas_Jugador;
+    span_Vidas_Enemigo.innerHTML = vidas_Enemigo;
+
+    let sectionMensajes = document.getElementById("mensajes")
+    let vidas = document.createElement("p")
+    vidas.innerHTML = "Tus vidas son: " + vidas_Jugador + ", y las vidas del enemigo son: " + vidas_Enemigo;
+    sectionMensajes.appendChild(vidas)
+
+    let spanResultado = document.getElementById("resultado")
+    let spanAtaque = document.getElementById("span.ataque")
+    let escenario = "Tu mascota atacó con " + ataqueJugador
+        + " y la mascota del enemigo atacó con "
+        + ataqueEnemigo
+    spanAtaque.innerHTML = escenario;
+    spanResultado.innerHTML = resultado;
+}
 
 
 function ataqueFuego() {
-    let spanAtaque = document.getElementById("span.ataque")
-    ataqueJugador = "FUEGO"
-    spanAtaque.innerHTML = ataqueJugador
     ataque_Enemigo()
+    ataqueJugador = "FUEGO"
 }
 
 function ataqueAgua() {
-    let spanAtaque = document.getElementById("span.ataque")
-    ataqueJugador = "AGUA"
-    spanAtaque.innerHTML = ataqueJugador
     ataque_Enemigo()
+    ataqueJugador = "AGUA"
 }
 
 function ataqueTierra() {
-    let spanAtaque = document.getElementById("span.ataque")
-    ataqueJugador = "TIERRA"
-    spanAtaque.innerHTML = ataqueJugador
     ataque_Enemigo()
+    ataqueJugador = "TIERRA"
+
 }
 
 function ataque_Enemigo() {
@@ -34,30 +67,31 @@ function ataque_Enemigo() {
     } else if (ataqueAleatorio == 3) {
         ataqueEnemigo = "TIERRA"
     }
-    let spanAtaque = document.getElementById("ataque_Enemigo")
-    spanAtaque.innerHTML = ataqueEnemigo
 }
 
 function aleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+/*Aquí se ubican todos los botones que dan inicio al juego, 
+desde seleccionar mascota y poderes, hasta dar inicio al combate.*/
+
 function inciar_juego() {
     let btn_mascota_jugador = document.getElementById("btn.mascota")
     btn_mascota_jugador.addEventListener("click", seleccionar_mascota_jugador)
-
     let btn_fuego = document.getElementById("btn.fuego")
     btn_fuego.addEventListener("click", ataqueFuego)
     let btn_tierra = document.getElementById("btn.tierra")
     btn_tierra.addEventListener("click", ataqueTierra)
     let btn_agua = document.getElementById("btn.agua")
     btn_agua.addEventListener("click", ataqueAgua)
-
+    let btn_combate = document.getElementById("pelear")
+    btn_combate.addEventListener("click", combate)
 }
 
 
 function seleccionar_mascota_jugador() {
-
+    let numero = 0;
     let inputHipodoge = document.getElementById("Hipodoge")
     let inputCapipepo = document.getElementById("Capipepo")
     let inputRatigueya = document.getElementById("Ratigueya")
@@ -67,25 +101,31 @@ function seleccionar_mascota_jugador() {
     if (inputHipodoge.checked) {
         alert("Seleccionaste a Hipodoge")
         spanJugador.innerHTML = "Hipodoge,"
+        numero = 1
     }
     else if (inputCapipepo.checked) {
         alert("Seleccionaste a Capipepo")
         spanJugador.innerHTML = "Capipepo,"
+        numero = 2
     }
     else if (inputRatigueya.checked) {
         alert("Seleccionaste a Ratigueya")
         spanJugador.innerHTML = "Ratigueya,"
+        numero = 3
     }
     else {
         alert("Porfa, seleccioná a un animal")
     }
 
-    seleccionar_mascota_enemigo()
+    seleccionar_mascota_enemigo(numero)
 }
 
-function seleccionar_mascota_enemigo() {
+function seleccionar_mascota_enemigo(valor) {
+    let mascotaAleatorio;
+    do {
+        mascotaAleatorio = aleatorio(1, 3)
+    } while (mascotaAleatorio == valor)
 
-    let mascotaAleatorio = aleatorio(1, 3);
     let spanEnemigo = document.getElementById("mascota_enemigo");
 
 
